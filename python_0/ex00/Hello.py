054b427087226f8010ae99b8ab6c3f1524cce8a8
Paulo <paulobernardo.2002@gmail.com>
2026-03-04 11:50:14 +0000

    1st commit

---
+++ python_0/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Portugal!")
+ft_set = {"Hello", "Lisbon!"}
+ft_dict = {"Hello" : "42 Lisboa!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ python_0/ex01/format_ft_time.py
@@ -0,0 +1,4 @@
+import datetime
+now = datetime.datetime.now()
+begin = datetime.datetime.(1970, 1, 1)
+print(f"Seconds since January 1, 1970: {now} begin: {begin}")
